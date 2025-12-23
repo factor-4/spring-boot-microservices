7a923259a10c1a52b4a13e39878f720bfa2bc5c9
factor-4 <snk.sojib@gmail.com>
2025-12-23 19:25:02 +0200

    product microservice postgresql throgh neon added

--- user/src/main/java/com/spring_ecom/user/config/MongoConfig.java
+++ user/src/main/java/com/spring_ecom/user/config/MongoConfig.java
@@ -1,13 +1,35 @@
 package com.spring_ecom.user.config;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
-public class MongoConfig extends AbstractMongoClientConfiguration {
+public class MongoConfig {
 
-    @Override
-    protected String getDatabaseName() {
-        return "userdb";
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
+    @Bean
+    public MongoClient mongoClient() {
+        System.out.println("🔗 Creating MongoClient with URI: " +
+                mongoUri.replaceAll("://([^:]+):([^@]+)@", "://***:***@"));
+
+        ConnectionString connectionString = new ConnectionString(mongoUri);
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+
+        return MongoClients.create(settings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), "userdb");
     }
 }
