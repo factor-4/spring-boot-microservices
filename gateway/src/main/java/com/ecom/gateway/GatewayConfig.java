91969842798fe688d0f022e5bc06cdc7ccebd72a
factor-4 <snk.sojib@gmail.com>
2026-01-24 18:46:31 +0200

    rate limiting with gateway

--- gateway/src/main/java/com/ecom/gateway/GatewayConfig.java
+++ gateway/src/main/java/com/ecom/gateway/GatewayConfig.java
@@ -1,20 +1,38 @@
 package com.ecom.gateway;
 
 
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class GatewayConfig {
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter(){
+        return new RedisRateLimiter(10,20,1);
+    }
+
+    @Bean
+    public KeyResolver hostNameKeyResolver(){
+        return exchange -> Mono.just(
+                exchange.getRequest().getRemoteAddress().getHostName());
+    }
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
         return builder.routes()
                 .route("product-service", r-> r
                         .path("/api/products/**")
-                        .filters(f-> f.circuitBreaker(config ->config
+                        .filters(f-> f
+                                .requestRateLimiter(config -> config
+                                        .setRateLimiter(redisRateLimiter())
+                                        .setKeyResolver(hostNameKeyResolver()))
+                                .circuitBreaker(config ->config
                                 .setName("ecomCircuitbreaker")
                                 .setFallbackUri("forward:/fallback/products")))
 //                        .filters(f->f.rewritePath("/products(?<segment>/?.*)",
