405d1f68b94273f4ab2b7c91ec276d76cb139b7e
factor-4 <snk.sojib@gmail.com>
2026-01-25 20:39:51 +0200

    consuming message from RabbitMQ

---
+++ notification/src/main/java/com/ecom/notification/OrderEventConsumer.java
@@ -0,0 +1,18 @@
+package com.ecom.notification;
+
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class OrderEventConsumer {
+    @RabbitListener(queues = "${rabbitmq.queue.name}")
+    public void handleOrderEvent(Map<String, Object> orderEvent){
+
+        System.out.println("Received order event " + orderEvent);
+
+
+    }
+}
