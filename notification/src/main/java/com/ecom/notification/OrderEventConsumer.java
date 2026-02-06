f9ad752fc8f9b204b9e1d12a8ebd51b9724d7827
factor-4 <snk.sojib@gmail.com>
2026-02-06 19:41:42 +0200

    notification receiver to spring cloud stream

--- notification/src/main/java/com/ecom/notification/OrderEventConsumer.java
+++ notification/src/main/java/com/ecom/notification/OrderEventConsumer.java
@@ -2,18 +2,32 @@ package com.ecom.notification;
 
 
 import com.ecom.notification.payload.OrderCreatedEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.function.Consumer;
 
 @Service
+@Slf4j
 public class OrderEventConsumer {
-    @RabbitListener(queues = "${rabbitmq.queue.name}")
-    public void handleOrderEvent(OrderCreatedEvent orderEvent){
+//    @RabbitListener(queues = "${rabbitmq.queue.name}")
+//    public void handleOrderEvent(OrderCreatedEvent orderEvent){
+//
+//        System.out.println("Received order eventupdated " + orderEvent);
+//
+//
+//    }
 
-        System.out.println("Received order eventupdated " + orderEvent);
+    @Bean
+    public Consumer<OrderCreatedEvent> orderCreated(){
 
+        return event -> {
+            log.info("Received order created event for order: {}", event.getOrderId());
+            log.info("Received order created event for order: {}", event.getUserId());
+        };
 
     }
 }
